ee5ad8e628b1556689a2285f9cc7dc0a881ab257
Fra <frawolf@outlook.it>
2021-01-23 09:48:06 +0100

    Made some responsive changes on Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,19 @@
-import React, { Component, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Home = () => {
-
     return (
-        <div className="flex space-x-5 mx-auto justify-center items-center">
+        <div className="space-x-5 mx-auto justify-center items-center lg:flex md:flex">
             <img
                 class="w-52 h-52 object-cover rounded-full"
                 src="https://frawolf.it/bot/api/156851568975675392"
                 alt="Me :)"
             />
             <div className="text-space">
-                <h1 className="text-7xl text-white font-bold max-w-6xl">
+                <h1 className="text-7xl text-white font-bold max-w-7xl">
                     <span className="text-transparent bg-gradient-to-r bg-clip-text from-blue-500 to-red-500">Francesco<br />Lombardo</span>
                 </h1>
                 <br />
-                <h1 className="text-4xl text-white font-bold max-w-6xl">
+                <h1 className="text-4xl text-white font-bold max-w-4xl">
                     Junior Backend Developer.
                 </h1>
             </div>
